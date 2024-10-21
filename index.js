e86a479f2bd541fab8fd882900db021a32d67dd5
Abhishek Mishra <rajtilak20@yhaoo.com>
2024-10-21 10:47:24 +0530

    added player in game

--- index.js
+++ index.js
@@ -5,32 +5,73 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Boundary {
+    static width =40
+    static height = 40
     constructor({position}) {
         this.position = position
         this.width = 40
         this.height = 40
     }
     draw() {
-        c.fillStyle = 'orange'
+        c.fillStyle = 'blue'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
-const boundries = [
-    new Boundary({
-        position: {
-            x: 10,
-            y: 10
-        }
-    }),
-    new Boundary({
-        position: {
-            x: 51,
-            y: 10
+class Player {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill()
+        c.closePath()
+    }
+}
+
+const map = [   
+    ['-','-','-','-','-','-',],
+    ['-',' ',' ',' ',' ','-',],
+    ['-',' ','-','-',' ','-',],
+    ['-',' ',' ',' ',' ','-',],
+    ['-','-','-','-','-','-',]
+]
+const boundries = []
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2, 
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+    })   
+
+map.forEach((row, i) => {
+    row.forEach((Symbol, j) =>{
+        switch (Symbol) {
+            case '-':
+                boundries.push(
+                    new Boundary({
+                    position: {
+                        x: Boundary.width * j,
+                        y: Boundary.height * i
+                    }
+                })
+            )
+                break
         }
     })
-]
+})
+
 
 boundries.forEach((boundary) => {
     boundary.draw()
 });
+player.draw()
